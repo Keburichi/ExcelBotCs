6c7682546180c281030d7ac276acdfbfb9b539bf
Carim A <carim@glaciate.net>
2025-06-15 02:01:00 +0100

    who is online command

--- Commands/RandomJobCommand.cs
+++ Commands/RandomJobCommand.cs
@@ -1,28 +1,101 @@
 using Discord.Interactions;
+using Discord.WebSocket;
 using ExcelBotCs.Discord;
 
 namespace ExcelBotCs.Commands;
 
+public static class ExcelExtensions
+{
+	private const ulong OfficerRoleId = 797521763214557204;
+	private const ulong FcRoleId = 797810200269684787;
+	private const ulong FcFriendRoleId = 810208611472637973;
+
+	public static ulong GuildId(this DiscordSocketClient client) => 797520687941943336;
+	public static SocketGuild ExcelGuild(this DiscordSocketClient client) => client.Guilds.First(x => x.Id == client.GuildId());
+	public static bool IsMember(this IReadOnlyCollection<SocketRole> roles) => roles.Any(role => role.Id == FcRoleId);
+	public static bool IsOfficer(this IReadOnlyCollection<SocketRole> roles) => roles.Any(role => role.Id == OfficerRoleId);
+	public static bool IsFriendOfFc(this IReadOnlyCollection<SocketRole> roles) => roles.Any(role => role.Id == FcFriendRoleId);
+}
+
+public class StreamAnnouncements
+{
+	private readonly DiscordBotService _discord;
+
+	public StreamAnnouncements(DiscordBotService discord)
+	{
+		_discord = discord;
+	}
+
+	public async Task CollectStreams()
+	{
+		var guild = _discord.Client.ExcelGuild();
+
+		await guild.DownloadUsersAsync();
+
+		var users = guild
+			.Users
+			.Where(user => user.Roles.IsMember())
+			.Select(user => (user, user.Activities));
+
+		foreach (var (user, activities) in users)
+		{
+			if (activities.Count == 0)
+				continue;
+
+			foreach (var activity in activities)
+			{
+				Console.WriteLine(activity.GetType().Name);
+				Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(activity));
+			}
+		}
+	}
+}
+
 public class RandomJobCommand : InteractionModuleBase<SocketInteractionContext>
 {
-	private const ulong ExcelDiscord = 797520687941943336;
 	private readonly DiscordBotService _discord;
+	private readonly StreamAnnouncements _streamAnnouncements;
 
 	public RandomJobCommand(DiscordBotService discord)
 	{
 		_discord = discord;
 		discord.Client.Ready += ClientOnReady;
+		_streamAnnouncements = new StreamAnnouncements(discord);
 	}
 
 	private async Task ClientOnReady()
 	{
-		await _discord.Interaction.RegisterCommandsToGuildAsync(ExcelDiscord);
+		await _discord.Interaction.RegisterCommandsToGuildAsync(_discord.Client.GuildId());
 	}
 
-	[SlashCommand("test", "Does nothing but reply \"Hello!\"")]
+	[SlashCommand("whoisingame", "Tells you who is currently in-game")]
+	public async Task WhoIsInGame()
+	{
+		var guild = _discord.Client.ExcelGuild();
+
+		await guild.DownloadUsersAsync();
+
+		var users = guild
+			.Users
+			.Where(user => user.Activities.Any(activity => activity.Name == "FINAL FANTASY XIV Online"))
+			.ToList();
+
+		if (users.Count == 0)
+		{
+			await RespondAsync("Nobody is currently logged in to FFXIV");
+			return;
+		}
+
+		await RespondAsync($"{users.Count} user{(users.Count == 1 ? " is" : "s are")} currently logged in to FFXIV: {string.Join(", ", users.Select(user => $"<@{user.Id}>"))}");
+	}
+
+	[SlashCommand("test", "Does nothing but reply \"Hello!\" (or whatever Zahrymm is currently testing in the moment by forcing it to run)")]
 	public async Task TestCommand()
 	{
-		await RespondAsync("Hello!");
+		await WhoIsInGame();
+
+		//await RespondAsync("Hello!");
+		//await _streamAnnouncements.CollectStreams();
 	}
 
 	private async Task RandomJob(params string[] jobs) => await RespondAsync($"I picked {jobs.PickRandom()} for you!");
@@ -43,7 +116,7 @@ public class RandomJobCommand : InteractionModuleBase<SocketInteractionContext>
 	public async Task RandomTank() => await RandomJob("Paladin", "Warrior", "Dark Knight", "Gunbreaker");
 
 	[SlashCommand("randomjob", "Gives you a random job")]
-	public async Task RandomJob() => await RandomJob("Dragoon", "Monk", "Ninja", "Samurai", "Reaper", "Viper", 
+	public async Task RandomJob() => await RandomJob("Dragoon", "Monk", "Ninja", "Samurai", "Reaper", "Viper",
 		"Black Mage", "Summoner", "Red Mage", "Pictomancer",
 		"Bard", "Machinist", "Dancer",
 		"White Mage", "Scholar", "Astrologian", "Sage",
@@ -56,8 +129,8 @@ public class RandomJobCommand : InteractionModuleBase<SocketInteractionContext>
 		var total = (minutes * 60) + seconds;
 		var emboldenUses = (int)Math.Floor(total / 120f);
 		var manaficUses = (int)Math.Floor(total / 110f);
-		
-		await RespondAsync(emboldenUses != manaficUses 
+
+		await RespondAsync(emboldenUses != manaficUses
 			? "You should use Manafication on cooldown to gain the maximum amount of uses."
 			: "You should align Manafication with Embolden. Use it 1 or 5 seconds before Embolden to fit 6 finishers into buffs.");
 	}
--- Discord/DiscordBotService.cs
+++ Discord/DiscordBotService.cs
@@ -17,7 +17,7 @@ public class DiscordBotService : BackgroundService
 	{
 		Client = new DiscordSocketClient(new DiscordSocketConfig()
 		{
-			GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+			GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent | GatewayIntents.GuildMembers | GatewayIntents.GuildPresences
 		});
 		Interaction = new InteractionService(Client);
 		_config = config;
