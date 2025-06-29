9c683ff2495ab83530d7aec039d061d1d494c239
Carim A <carim@glaciate.net>
2025-06-29 15:44:59 +0100

    Signups command

--- Modules/TeamFormation/TeamFormationInteraction.cs
+++ Modules/TeamFormation/TeamFormationInteraction.cs
@@ -3,24 +3,84 @@ using Discord.Interactions;
 
 namespace ExcelBotCs.Modules.TeamFormation;
 
+[Group("team", "Team commands")]
 public class TeamFormationInteraction : InteractionModuleBase<SocketInteractionContext>
 {
+	public enum Role
+	{
+		Tank,
+		Healer,
+		Melee,
+		Caster,
+		Ranged
+	}
+
 	private const string TankRoleEmote = "<:RoleTank:1380979172423499846>";
 	private const string HealerRoleEmote = "<:RoleHealer:1380979170787721368>";
 	private const string MeleeRoleEmote = "<:RoleMelee:873621778214318091>";
 	private const string CasterRoleEmote = "<:RoleCaster:873621778566635540>";
 	private const string RangedRoleEmote = "<:RoleRanged:873621778453368895>";
 
-	private readonly List<(string Role, Emote Emote, int Minimum, int Maximum)> _groups =
-	[
-		("Tank", TankRoleEmote, 2, 2),
-		("Healer", HealerRoleEmote, 2, 2),
-		("Melee", MeleeRoleEmote, 1, 2),
-		("Caster", CasterRoleEmote, 1, 2),
-		("Ranged", RangedRoleEmote, 1, 1)
-	];
+	private readonly Dictionary<Role, (Emote Emote, int Minimum, int Maximum)> _groups =
+		new()
+		{
+			{ Role.Tank, (TankRoleEmote, 2, 2) },
+			{ Role.Healer, (HealerRoleEmote, 2, 2) },
+			{ Role.Melee, (MeleeRoleEmote, 1, 2) },
+			{ Role.Caster, (CasterRoleEmote, 1, 2) },
+			{ Role.Ranged, (RangedRoleEmote, 1, 1) }
+		};
 
-	[SlashCommand("makegroup", "Pick a number and have a chance to win!")]
+	private async Task<Dictionary<Role, HashSet<ulong>>> GetSignupsFromMessage(IMessage message)
+	{
+		var signups = new Dictionary<Role, HashSet<ulong>>()
+		{
+			{ Role.Tank, [] },
+			{ Role.Healer, [] },
+			{ Role.Melee, [] },
+			{ Role.Caster, [] },
+			{ Role.Ranged, [] }
+		};
+
+		foreach (var (role, (emote, _, _)) in _groups)
+		{
+			var users = await message.GetReactionUsersAsync(emote, 100).FlattenAsync();
+			foreach (var user in users)
+			{
+				signups[role].Add(user.Id);
+			}
+		}
+
+		return signups;
+	}
+
+
+	[SlashCommand("list", "Get a list of signups from the post provided")]
+	public async Task GetSignups(string postUrl)
+	{
+		await DeferAsync();
+
+		switch (await Context.Client.GetMessageFromUrl(postUrl))
+		{
+			case Extensions.NotValidUrlMessageResponse:
+				await FollowupAsync("The provided URL does not seem to be a valid Discord URL", ephemeral: true);
+				break;
+
+			case Extensions.NotFoundUrlMessageResponse:
+				await FollowupAsync(
+					"Could not find the Guild/Channel this message belongs to. Do I have permission to view it?",
+					ephemeral: true);
+				break;
+
+			case Extensions.SuccessMessageResponse msg:
+				var group = await GetSignupsFromMessage(msg.Message);
+				await FollowupAsync($"### Signups from {postUrl}\n{GenerateRoleText(group, Role.Tank)}{GenerateRoleText(group, Role.Healer)}{GenerateRoleText(group, Role.Melee)}{GenerateRoleText(group, Role.Caster)}{GenerateRoleText(group, Role.Ranged)}");
+				break;
+		}
+	}
+
+
+	[SlashCommand("make", "Randomly assemble a group from the post provided")]
 	public async Task MakeGroup(string postUrl)
 	{
 		await DeferAsync();
@@ -38,32 +98,19 @@ public class TeamFormationInteraction : InteractionModuleBase<SocketInteractionC
 				break;
 
 			case Extensions.SuccessMessageResponse msg:
+				var signups = await GetSignupsFromMessage(msg.Message);
 
-				var reactions = msg.Message.Reactions;
-				var signups = new List<(string Role, ulong Id)>();
-
-				// check that each role react is present
-				foreach (var (role, emote, required, _) in _groups)
+				if (signups.Values.Sum(set => set.Count) < 8)
 				{
-					var reaction = reactions.TryGetValue(emote, out var metadata);
-					if (!reaction || metadata.ReactionCount < required)
-					{
-						await RespondAsync($"There are not enough {role} signups to form a group.");
-						return;
-					}
-
-					var msgReactions = await msg.Message.GetReactionUsersAsync(emote, 100).FlattenAsync();
-					var toAdd = msgReactions.Select(user => (role, user.Id))
-						.ToList();
-
-					signups.AddRange(toAdd);
+					await RespondAsync("There are not enough signups to form a group.");
+					return;
 				}
 
-				Dictionary<string, List<ulong>>? group = null;
+				Dictionary<Role, HashSet<ulong>>? group = null;
 				var cts = new CancellationTokenSource();
-				var task = TryFormGroup(cts.Token, signups.ToList());
+				var task = TryFormGroup(cts.Token, signups);
 
-				if (await Task.WhenAny(task, Task.Delay(TimeSpan.FromSeconds(10), cts.Token)) == task)
+				if (await Task.WhenAny(task, Task.Delay(TimeSpan.FromSeconds(5), cts.Token)) == task)
 				{
 					await cts.CancelAsync();
 					group = await task;
@@ -74,34 +121,35 @@ public class TeamFormationInteraction : InteractionModuleBase<SocketInteractionC
 					await FollowupAsync("Trying to generate a group took too long, it's probably not possible to form one.", ephemeral: true);
 				}
 
-				await FollowupAsync($"### Group generated from {postUrl}\n{GenerateRoleText(TankRoleEmote, group, "Tank")}{GenerateRoleText(HealerRoleEmote, group, "Healer")}{GenerateRoleText(MeleeRoleEmote, group, "Melee")}{GenerateRoleText(CasterRoleEmote, group, "Caster")}{GenerateRoleText(RangedRoleEmote, group, "Ranged")}");
+				await FollowupAsync($"### Group generated from {postUrl}\n{GenerateRoleText(group, Role.Tank)}{GenerateRoleText(group, Role.Healer)}{GenerateRoleText(group, Role.Melee)}{GenerateRoleText(group, Role.Caster)}{GenerateRoleText(group, Role.Ranged)}");
 				break;
 		}
 	}
 
-	private string GenerateRoleText(string emote, Dictionary<string, List<ulong>> group, string role) =>
-		$"{emote}: {string.Join(' ', group[role].Select(id => $"<@{id}>"))}\n";
+	private string GenerateRoleText(Dictionary<Role, HashSet<ulong>> group, Role role) =>
+		$"{_groups[role].Emote}: {string.Join(' ', group[role].Select(id => $"<@{id}>"))}\n";
 
-	private async Task<Dictionary<string, List<ulong>>?> TryFormGroup(CancellationToken token, List<(string Role, ulong Id)> signups)
+	private async Task<Dictionary<Role, HashSet<ulong>>?> TryFormGroup(CancellationToken token, Dictionary<Role, HashSet<ulong>> signups)
 	{
 		while (true)
 		{
 			if (token.IsCancellationRequested)
 				return null;
 
-			var result = await FormGroup(token, signups.ToList());
+			var result = await FormGroup(token, signups);
 			if (result != null)
 				return result;
 		}
 	}
 
-	private async Task<Dictionary<string, List<ulong>>?> FormGroup(CancellationToken token, List<(string Role, ulong Id)> signups)
+	private async Task<Dictionary<Role, HashSet<ulong>>?> FormGroup(CancellationToken token, Dictionary<Role, HashSet<ulong>> signups)
 	{
-		var composedGroup = new Dictionary<string, List<ulong>>();
+		var clone = signups.ToDictionary();
+		var composedGroup = new Dictionary<Role, HashSet<ulong>>();
 
-		foreach (var group in _groups)
+		foreach (var (role, _) in _groups)
 		{
-			composedGroup.Add(group.Role, new List<ulong>());
+			composedGroup.Add(role, []);
 		}
 
 		while (composedGroup.Values.SelectMany(item => item).Count() != 8)
@@ -109,42 +157,33 @@ public class TeamFormationInteraction : InteractionModuleBase<SocketInteractionC
 			if (token.IsCancellationRequested)
 				return null;
 
-			// start with the role with the lowest signups for roles that haven't yet been selected
-			var lowestReaction = signups
-				.GroupBy(x => x.Role)
-				.OrderBy(x => x.Count())
-				.Select(x => x.Key)
-				.FirstOrDefault(x => !composedGroup.TryGetValue(x, out var list) || list.Count < 1);
+			var min = clone.Min(kvp => kvp.Value.Count);
+			var nextPriority = clone
+				.Where(kvp => kvp.Value.Count == min)
+				.SelectMany(kvp => kvp.Value.Select(id => (kvp.Key, id)))
+				.ToList();
 
-			// if no such case exists, flex
-			lowestReaction ??= signups.GroupBy(x => x.Role)
-				.OrderBy(x => x.Count())
-				.Select(x => x.Key)
-				.FirstOrDefault();
-
-			if (lowestReaction == null)
+			if (nextPriority.Count == 0)
 				return null;
 
-			var matchingSignups = signups.Where(x => x.Role == lowestReaction);
+			var user = nextPriority.Shuffle().First();
 
-			var user = matchingSignups.Shuffle().First();
-
-			if (!composedGroup.TryAdd(user.Role, [user.Id]))
-				composedGroup[user.Role].Add(user.Id);
+			if (!composedGroup.TryAdd(user.Key, [user.id]))
+				composedGroup[user.Key].Add(user.id);
 
 			// remove user from the pool
-			signups.RemoveAll(signup => signup.Id == user.Id);
+			clone[user.Key].Remove(user.id);
 
 			// if we have enough, remove the role from the pool
-			if (composedGroup[user.Role].Count >= _groups.First(group => group.Role == user.Role).Maximum)
-				signups.RemoveAll(signup => signup.Role == user.Role);
+			if (composedGroup[user.Key].Count >= _groups[user.Key].Maximum)
+				clone.Remove(user.Key);
 
 			// if we have enough dps, remove them all from the pool to prevent 5 dps nonsense
-			if (composedGroup["Melee"].Count + composedGroup["Caster"].Count + composedGroup["Ranged"].Count >= 4)
+			if (composedGroup[Role.Melee].Count + composedGroup[Role.Caster].Count + composedGroup[Role.Ranged].Count >= 4)
 			{
-				signups.RemoveAll(signup => signup.Role == "Melee");
-				signups.RemoveAll(signup => signup.Role == "Caster");
-				signups.RemoveAll(signup => signup.Role == "Ranged");
+				clone.Remove(Role.Melee);
+				clone.Remove(Role.Caster);
+				clone.Remove(Role.Ranged);
 			}
 
 			await Task.Yield();
